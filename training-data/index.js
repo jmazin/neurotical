b1327fd69884170e5c55ad76b58bcf4cad442c6a
Jonathan Mazin <jonathan.mazin@gmail.com>
2025-04-24 23:54:36 -0400

    add datasets (circles, sprials)

--- training-data/index.js
+++ training-data/index.js
@@ -2,3 +2,5 @@ export * as line from "./line.js";
 export * as "2 points" from "./two-points.js";
 export * as magical from "./magical.js";
 export * as curvy from "./curvy.js";
+export * as circles from "./circles.js";
+export * as spiral from "./spiral.js";
